2f26b2a2d359ee236dfb25fedf022ca0c2c34c85
Dhiraj Sapkota <101037569+Dheeruvaii@users.noreply.github.com>
2024-02-04 10:28:21 +0000

    update models_fields

--- Montessori_Project/montessori_app/models.py
+++ Montessori_Project/montessori_app/models.py
@@ -1,8 +1,13 @@
 """this is my montessori project models"""
 from django.db import models
 
+
 class Organization(models.Model):
-    org_name=models.CharField(max_length=50)
+    org_name=models.CharField(max_length=50,unique=True)
+    organization_category = models.CharField(max_length=200, unique=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.org_name
 
@@ -10,6 +15,8 @@ class Organization(models.Model):
 class Teacher(models.Model):
     username=models.CharField(max_length=20)
     subject = models.CharField(max_length=50)
+    Permanent_address=models.CharField(max_length=20)
+    temporary_address=models.CharField(max_length=20)
 
     def __str__(self):
         return self.subject
@@ -18,6 +25,10 @@ class Teacher(models.Model):
 class Student(models.Model):
     username=models.CharField(max_length=20)
     grade = models.CharField(max_length=10)
+    father_name=models.CharField(max_length=20)
+    Mother_name=models.CharField(max_length=20)
+    Permanent_address=models.CharField(max_length=20)
+    temporary_address=models.CharField(max_length=20)
 
     def __str__(self):
         return self.username
@@ -28,13 +39,13 @@ class Mark(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     subject = models.CharField(max_length=50)
     score = models.DecimalField(max_digits=5, decimal_places=2)
+    checked_by=models.ForeignKey(Teacher,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.student
 
 
 class InventoryItem(models.Model):
-    # name = models.CharField(max_length=100)
     item_name = models.CharField(max_length=100)
     item_code = models.CharField(max_length=10)
     quantity = models.PositiveIntegerField()
--- Montessori_Project/montessori_app/urls.py
+++ Montessori_Project/montessori_app/urls.py
@@ -4,7 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('v1/org/', OrganizationApiView.as_view(), name='org-list'),
-
     path('teachers/', TeacherListCreateView.as_view(), name='teacher-list-create'),
     path('students/', StudentListCreateView.as_view(), name='student-list-create'),
     path('marks/', MarkListCreateView.as_view(), name='mark-list-create'),
